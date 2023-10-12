d3c0ac4de3700c593d048d9451049fcd755504a9
nikkodote-ai <nikkodote.ai@gmail.com>
2023-10-12 18:55:52 +1100

    added links

--- frontend/src/components/ContactMe.jsx
+++ frontend/src/components/ContactMe.jsx
@@ -1,5 +1,5 @@
 import { GitHub, Email, Instagram, LinkedIn} from '@mui/icons-material'
-import { Box, Stack} from '@mui/material';
+import { Box, Button, Link, Stack} from '@mui/material';
 import {motion} from 'framer-motion';
 
 
@@ -8,10 +8,25 @@ export const ContactMe = () => {
     <Box  textAlign={"center"} style={{color: 'white'}} m={0} display="grid">
     {/* <Typography>Contact Me! <KeyboardDoubleArrowDown/> </Typography> */}
         <Stack direction="row" justifyContent={"center"} alignContent={"center"}  spacing={8}>
+          <a href={`mailto:nikko.dote@gmail.com`} rel="noopener noreferrer">
+
    <Email  fontSize='large'/>
+          </a>
+            
+            <a href="https://www.linkedin.com/in/nikkodote" rel="noopener noreferrer">
     <LinkedIn fontSize="large" />
+
+            </a>
+
+          <a href="https://github.com/nikkodote-ai" rel="noopener noreferrer">
     <GitHub fontSize="large" />
+            
+          </a>
+
+          <a href="https://www.instagram.com/thenikkodote/">
+            
     <Instagram fontSize="large" />
+          </a>
 
             </Stack>
     </Box>
